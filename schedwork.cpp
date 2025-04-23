1a72869586eba6a6edeff28db9d855f0c80c5ec3
Olivia Liau <oliau@usc.edu>
2025-04-23 08:33:08 +0000

    first

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,9 +21,54 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool schedHelper(
+  const AvailabilityMatrix&,
+  const size_t,
+  const size_t,
+  DailySchedule&,
+  vector<size_t>&,
+  size_t, size_t
+);
 
 // Add your implementation of schedule() and other helper functions here
+bool schedHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t>& shiftsTaken,
+    size_t days, 
+    size_t shiftsOfDay)
+{
+    if (days == sched.size()) return true;
+
+    for (size_t worker = 0; worker < avail[days].size(); ++worker)
+    {
+        if (avail[days][worker] && shiftsTaken[worker] < maxShifts
+        && find(sched[days].begin(), sched[days].end(), worker) == sched[days].end()) {
+
+            sched[days][shiftsOfDay] = worker;
+            shiftsTaken[worker]++;
+
+            if (shiftsOfDay + 1 == dailyNeed) {
+                if (schedHelper(avail, dailyNeed, maxShifts, sched, shiftsTaken, days+1, 0))
+                    return true;
+            }
+
+            else { 
+                if (schedHelper(avail, dailyNeed, maxShifts, sched, shiftsTaken, days, shiftsOfDay+1))
+                    return true;
+            }
+
+            sched[days][shiftsOfDay] = INVALID_ID;
+            shiftsTaken[worker]--;
+
+        }
+    }
+    return false;   
+}
+
+
 
 bool schedule(
     const AvailabilityMatrix& avail,
@@ -35,11 +80,17 @@ bool schedule(
     if(avail.size() == 0U){
         return false;
     }
-    sched.clear();
     // Add your code below
+    size_t days = avail.size();
+    size_t shiftsOfDay = avail[0].size();
 
+    // cout << days << " " << shiftsOfDay;
 
+    vector<size_t> shiftsTaken(shiftsOfDay, 0);
 
+    sched.clear();
+    sched.resize(days, vector<Worker_T>(dailyNeed, INVALID_ID));
 
+    return schedHelper(avail, dailyNeed, maxShifts, sched, shiftsTaken, 0, 0);
 }
 
